401525c00307c4c7a2b479096be533858b96383e
Alejandra De Los Santos <delossantosa@jefatura.gob.ar>
2021-08-21 15:54:54 -0300

    installed eslint and styled components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import Photo from './coponents/Photo';
+import React from 'react'
+import './App.css'
+import Photo from './coponents/Photo'
 
-function App() {
+function App () {
   return (
     <div className="App">
       PIC OF THE DAY
       <Photo/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/coponents/Photo/index.jsx
+++ src/coponents/Photo/index.jsx
@@ -1,28 +1,38 @@
-import React, { useState, useEffect } from "react";
-// import fetchPhoto from "../../services/fetchPhoto";
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+// import fetchPhoto from '../../services/fetchPhoto';
 
-const apiKey = "api_key=Jr3NMgrEUirC5Yb18je6auQ5c8aUF3Lo9u4dqueO"
-const apiUrl = `https://api.nasa.gov/planetary/apod?${apiKey}`;
+const StyledPhoto = styled.div`
+    border: solid 2px red;
+`
+
+const apiKey = 'api_key=Jr3NMgrEUirC5Yb18je6auQ5c8aUF3Lo9u4dqueO'
+const apiUrl = `https://api.nasa.gov/planetary/apod?${apiKey}`
 
 const Photo = () => {
-    const [ photoData, setPhotoData ] = useState("");
-
+  const [photoData, setPhotoData] = useState('')
+  useEffect(() => {
     try {
-    fetch(apiUrl).then(response => response.json())
-        .then(response => {
-            setPhotoData(response);
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((response) => {
+          setPhotoData(response)
         })
-    } catch(e) {
-        console.log(e);
+      console.log('yeah')
+    } catch (e) {
+      console.log('ups')
     }
-    const { url, title, date, explanation } = photoData;
+  }, [])
 
-    return (
-        <div>
-            {title}
-        <img src={`${url}`} alt={title}/>
-        </div>
-    )
+  const { url, title, date, explanation } = photoData
+  return (
+    <div>
+      {title}
+      <StyledPhoto>
+      <img src={`${url}`} alt={title} />
+      </StyledPhoto>
+    </div>
+  )
 }
 
-export default Photo;
+export default Photo
