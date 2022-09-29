0d97d0db25da54d40c7a1d4fef5616ebabb23746
Aledeloss <Aledeloss@gmail.com>
2022-09-29 19:22:19 -0300

    Migrate to TS

---
+++ src/components/Photo/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyledExplanation, StyledPhoto, StyledPhotoTitle } from './styles';
+
+interface PhotoProps {
+  hdurl: string,
+  date: string,
+  title: string,
+  explanation: string
+};
+
+const Photo = ({ hdurl, title, date, explanation }: PhotoProps) => {
+  return (
+    <div>
+      <StyledPhotoTitle>{title}</StyledPhotoTitle>
+      <StyledPhoto loading="lazy" src={`${hdurl}`} alt={title} />
+      <StyledExplanation>{explanation}</StyledExplanation>
+      <div>Date: {date}</div>
+    </div>
+  );
+};
+
+export default Photo;
---
+++ src/components/Photo/styles.tsx
@@ -0,0 +1,20 @@
+
+import styled from 'styled-components';
+
+export const StyledPhotoTitle = styled.div`
+  margin-top: 0.5em;
+  font-size: 1.5em;
+`;
+
+export const StyledExplanation = styled.div`
+  margin-bottom: 0.5em;
+`;
+export const StyledPhoto = styled.img`
+    max-width: 100%;
+    border: solid 2px;
+    margin-top: 25px;
+    margin-bottom: 2%;
+    border: solid white 5px;
+    border-radius: 2px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
