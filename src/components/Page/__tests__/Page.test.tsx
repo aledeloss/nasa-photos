e7d3fb0cd78a191bdb2bcf375fff4c33408f88ff
Aledeloss <Aledeloss@gmail.com>
2022-10-18 23:57:52 -0300

    Add test

--- src/components/Page/__tests__/Page.test.tsx
+++ src/components/Page/__tests__/Page.test.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Page from '../';
 
-it('should render the page title', () => {
+const renderPage = () => {
   global.scrollTo = jest.fn();
   const history = createMemoryHistory();
   const route = '/whatever-the-route-is';
   history.push(route);
 
   const { getByText } = render(
-      <Router history={history}>
-        <Page/>
-      </Router>
+        <Router history={history}>
+          <Page/>
+        </Router>
   );
-  const linkElement = getByText(/RANDOM NASA PIC OF THE DAY/i);
-  expect(linkElement).toBeInTheDocument();
+  return { getByText };
+};
+
+it('should render the page title', () => {
+  const { getByText } = renderPage();
+  const titleText = getByText(/RANDOM NASA PIC OF THE DAY/i);
+  expect(titleText).toBeInTheDocument();
+});
+
+it('should render the page footer', () => {
+  const { getByText } = renderPage();
+  const footerText = getByText(/Made by/i);
+  expect(footerText).toBeInTheDocument();
 });
